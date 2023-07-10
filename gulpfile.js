e2aaeced0885a3eea6835d810880e94828bc3ac8
mrgreck <51150127+mrgreck@users.noreply.github.com>
2023-07-10 11:47:06 +0600

    Start
    настроил рабочую зону и начал версать заголовок

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+const {src,dest,watch, parallel} = require('gulp');
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const browsersync = require('browser-sync').create();
+
+
+function scripts(){
+    return src('src/js/**.js')
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('src/js'))
+    .pipe(browsersync.stream())
+}
+
+
+
+function styles(){
+	return src('src/scss/**.scss')
+    .pipe(concat('styles.min.css'))
+    .pipe(scss({outputStyle:'compressed'}))
+    .pipe(dest('src/css'))
+    .pipe(browsersync.stream())
+}
+
+function watching(){
+    watch(['src/scss/stayle.scss'],styles)
+    watch(['src/js/main.js'],scripts)
+    watch(['src/*.html']).on('change', browsersync.reload)
+}
+
+function browsersy(){
+    browsersync.init({
+        server:{
+            baseDir:"src/"
+        }
+    })
+}
+
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watching = watching;
+exports.browsersy = browsersy;
+
+exports.start = parallel(styles,scripts,browsersy,watching);
